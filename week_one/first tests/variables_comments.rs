72129fecc4a70939120a96d8ef7ecd7853f889b0
Siro <siro.knubel@gmail.com>
2022-02-03 15:11:59 +0100

    pushing some original files

---
+++ week_one/first tests/variables_comments.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    //this is an comment
+    /*
+    Dicks
+
+    */
+    println!("Hello, world!");
+    //cant change
+    let x = 45;
+    //can change
+    let mut y = 50;
+    println!("x = {} | y = {}", x, y);
+
+    y = 60;
+
+    println!("x = {} | y = {}", x, y);
+}
